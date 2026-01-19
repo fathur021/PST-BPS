8db90c5704dbe91cd0ace68d4e0579e459170122
Fathur <tuagsfathur79@gmail.com>
2026-01-19 11:05:40 +0700

    update vite 2

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/css/filament/user/theme.css",
+                "vendor/livewire/livewire/dist/livewire.js"
             ],
             refresh: [...refreshPaths, "app/Livewire/**"],
         }),
